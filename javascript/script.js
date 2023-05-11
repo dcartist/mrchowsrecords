e3f17eebc31d7beea32f42c5e5e2ce7549aaad02
dcartist <pbannerman13@gmail.com>
2023-05-11 15:53:08 -0400

    cleaning up webpage

--- javascript/script.js
+++ javascript/script.js
@@ -1,38 +1,67 @@
-console.log("Hello World!")
+
 let recordsCollection = document.querySelector('#records')
 console.log(recordsCollection)
 function insertRecord(information){
     console.log(information.release.artist)
 const newListingItem = document.createElement('li')
-
-const listingText_Description = document.createTextNode(information.release.description)
-
-newListingItem.innerHTML +=  `<h3> ${information.release.artist} </h3> <p> ${information.release.description} </p>`
-
-
-// newListingItem.innerHTML = recordBox
-// newListingItem.appendChild(listingText)
+newListingItem.innerHTML +=  `<h3> ${information.release.artist} </h3> <p class="recordParagraph"> ${information.release.description} </p> <a href="${information.uri}" target="_blank" rel="noopener noreferrer" class="recordButton"> for more information </a> `
 document.getElementById('records').appendChild(newListingItem)
-    // let recordSection = `<p> ${information.release.artist} </p>`
-    // recordsCollection.appendChild(recordSection)
 }
 
 
 
-fetch('https://api.discogs.com/users/100_BPM/inventory')
+function lastpage(pageNumber){
+    console.log(pageNumber)
+    fetch(`https://api.discogs.com/users/100_BPM/inventory?page=${pageNumber}&per_page=50`)
 .then(res => res.json())
 .then(data => {
     console.log("oompa")
-    console.log(data.listings)
+    console.log(data)
     data.listings.map(listing => {
         insertRecord(listing)
         console.log(listing)})
-// for (recordData in data.listings) {
-// insertRecord(recordData)
-//     console.log(data.listings[recordData])
+}
+    )
 
-// }
+}
 
+
+function gettingPages(numberEntered){
+    fetch(`https://api.discogs.com/users/100_BPM/inventory?page=${numberEntered}&per_page=50`)
+.then(res => res.json())
+.then(data => {
+    console.log("oompa")
+    console.log(data.listings.length)
+console.log(numberEntered)
+lastpage(numberEntered)
+    if (data.listings.length < 10){
+        lastpage(numberEntered - 1)
+        
+    }
+
+}
+    )
+}
+
+
+
+// fetch('https://api.discogs.com/users/100_BPM/inventory?page=13&per_page=50')
+fetch('https://api.discogs.com/users/100_BPM/inventory')
+.then(res => res.json())
+.then(data => {
+    // console.log("oompa")
+    // let page = data.pagination.pages-1
+    // console.log(page)
+    // console.log(data.pagination.pages)
+    // lastpage(data.pagination.pages)
+    gettingPages(data.pagination.pages)
+
+
+
+
+    // data.listings.map(listing => {
+    //     insertRecord(listing)
+    //     console.log(listing)})
 }
     )
 
